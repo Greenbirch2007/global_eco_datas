d8f001d8ba627b7b73313e52c3bf46349eb6c0f8
Greenbirch2007 <291109028@qq.com>
2018-06-21 22:39:50 +0800

    the third commit

--- eoc_data_spider.py
+++ eoc_data_spider.py
@@ -2,7 +2,7 @@
 
 import requests
 import re
-
+import json
 
 
 
@@ -27,16 +27,57 @@ html = call_page(url)
 
 
 #重点用正则提出标签，空格即可
-# 整体是一个列表，但是for迭代之后就变成了45个字符串
+# 整体是一个列表，但是for迭代之后就变成了45个字符串  print(a)
 
 # 还是要考虑用表达式整体去匹配，不能割裂然后再去拼接！ 模块处理又变成元组了
-
+# 尝试进行一对多字符串拼接组合
+# 11 44 484
 def parse_one_page(html):
-    patt = re.compile('.*?<td width="100">(.*?)</td><td>(.*?)</td>',re.S)   #标题匹配,注意去除换行(没有实现去除空格，但是标签都没了)
-    items = re.findall(patt,html)
-    print(items)
+    patt1 = re.compile(' <td width="100">(.*?)</td>',re.S)   #标题匹配,注意去除换行(没有实现去除空格，但是标签都没了)
+    patt2 = re.compile('<td>(.*?)</td>', re.S)
+    item1 =  re.findall(patt1,html)
+    item2 = re.findall(patt2,html)
+    # for k,v in zip(item1,item2[0:484:11]):
+    #     print(k,v)
+
+
+
+    #往空列表里面两次添加
+    # for i1 in item1:
+    #     print(i1)
+    # for i2 in item2:
+    #     print(len(i2))
+
+            # ulist.append(i2)
+
+
+
+    a0 = {item1[0]:item2[0:11]}
+    # a1 = {item1[1]: item2[12:23]}
+
+    print(a0)
+    # print(a1)
+
+
+
+#后面整理入mysql，不考虑到MongoDB，暂放 2018.6.21
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # for i in items:
     #     print(i)
+
     # print(items)
     # print(len(items))
 
